482b872ccbb33a9946f9897d570cab8878b235b8
Fleikcor <rock_meira@hotmail.com>
2021-02-01 14:36:30 -0300

    Add Type Hint and DistributionListAction methods

--- src/DiegoSouza/Zimbra/ZimbraApiClient.php
+++ src/DiegoSouza/Zimbra/ZimbraApiClient.php
@@ -4,21 +4,36 @@ namespace DiegoSouza\Zimbra;
 
 use Illuminate\Log\LogManager;
 use Illuminate\Support\Traits\ForwardsCalls;
+use Zimbra\Admin\AdminFactory;
+use Zimbra\Admin\Struct\DistributionListSelector;
+use Zimbra\Admin\Struct\DomainSelector;
+use Zimbra\Enum\AccountBy;
+use Zimbra\Enum\DistributionListBy;
+use Zimbra\Enum\DomainBy;
+use Zimbra\Enum\GranteeType;
+use Zimbra\Enum\Operation;
+use Zimbra\Enum\DistributionListGranteeBy;
+use Zimbra\Struct\AccountSelector;
+use Zimbra\Struct\KeyValuePair;
+use Zimbra\Account\Struct\DistributionListRightSpec;
+use Zimbra\Account\Struct\DistributionListGranteeSelector;
+use Zimbra\Account\Request\DistributionListAction;
+use Zimbra\Account\Struct\DistributionListAction as DLAction;
 
 class ZimbraApiClient
 {
     use ForwardsCalls;
 
-    public $api;
-    private $logger;
+    protected $api;
+    protected $logger;
     protected $domain;
 
-    public function __construct($host, $user, $password, LogManager $logger)
+    public function __construct(string $host, string $emailDomain, string $user, string $password, LogManager $logger)
     {
-        $this->domain = 'santos.sp.gov.br';
-        $this->api = \Zimbra\Admin\AdminFactory::instance("https://{$host}:7071/service/admin/soap");
+        $this->api = AdminFactory::instance("https://{$host}:7071/service/admin/soap");
         $this->api->auth($user, $password);
 
+        $this->domain = $emailDomain;
         $this->logger = $logger;
 
         $this->api->getClient()->on('before.request', function ($request) {
@@ -30,110 +45,195 @@ class ZimbraApiClient
         });
     }
 
-    public function createDistributionList($name)
-    {       
+    public function createDistributionList(string $distListName)
+    {
         $dynamic = false;
-        $attr = new \Zimbra\Struct\KeyValuePair('', '');       
-        return $this->api->createDistributionList("{$name}@{$this->domain}", $dynamic, [$attr]);
+        $attr = $this->createKeyPair();
+
+        return $this->api->createDistributionList("{$distListName}@{$this->domain}", $dynamic, [$attr]);
     }
 
-    public function deleteDistributionList($id)
+    public function deleteDistributionList(string $distListId)
     {
-        return $this->api->deleteDistributionList($id);
+        return $this->api->deleteDistributionList($distListId);
     }
 
     public function getAllDistributionLists()
     {
-        $domainSelector = new \Zimbra\Admin\Struct\DomainSelector(\Zimbra\Enum\DomainBy::NAME(), $this->domain);
+        $domainSelector = new DomainSelector(DomainBy::NAME(), $this->domain);
         return $this->api->getAllDistributionLists($domainSelector);
-    } 
-
-    public function addDistributionListMember($distListId, $users)
-    {
-        return $this->api->addDistributionListMember($listId, [$user]);
     }
 
-    public function getDistributionListById($id)
+    public function addDistributionListMember(string $distListId, array $users)
     {
-        $attr = new \Zimbra\Struct\KeyValuePair('', '');
-        $dl = new \Zimbra\Admin\Struct\DistributionListSelector(\Zimbra\Enum\DistributionListBy::ID(), $id);      
-        return $this->api->getDistributionList($dl, 0, 0, true, [$attr]);
+        return $this->api->addDistributionListMember($distListId, $users);
     }
 
-    public function getDistributionListByName($name)
-    {   
-        $attr = new \Zimbra\Struct\KeyValuePair('', '');
-        $dl = new \Zimbra\Admin\Struct\DistributionListSelector(\Zimbra\Enum\DistributionListBy::NAME(), $name);
-        return $this->api->getDistributionList($dl, 0, 0, true, [$attr]);
+    public function getDistributionListById(string $distListId)
+    {
+        $limit = 0;
+        $offset = 0;
+        $sortAscending = true;
+        $attr = $this->createKeyPair();
+
+        $dl = new DistributionListSelector(DistributionListBy::ID(), $distListId);
+
+        return $this->api->getDistributionList($dl, $limit, $offset, $sortAscending, [$attr]);
     }
 
-    public function getDistributionListMembership($distListName)
+    public function getDistributionListByName(string $distListName)
     {
-        $dl = new \Zimbra\Admin\Struct\DistributionListSelector(\Zimbra\Enum\DistributionListBy::Name(), $distListName);
-        return $this->api->getDistributionListMembership($dl, 100, 25);
+        $limit = 0;
+        $offset = 0;
+        $sortAscending = true;
+        $attr = $this->createKeyPair();
+
+        $dl = new DistributionListSelector(DistributionListBy::NAME(), $distListName);
+
+        return $this->api->getDistributionList($dl, $limit, $offset, $sortAscending, [$attr]);
+    }
+
+    public function GrantSendAs(string $distListName, string $emailAccountName)
+    {
+        $grantee = new DistributionListGranteeSelector(GranteeType::USR(), DistributionListGranteeBy::NAME(), $emailAccountName);
+        $right = new DistributionListRightSpec("sendAsDistList", [$grantee]);
+        $dl = new DistributionListSelector(DistributionListBy::Name(), $distListName);
+        $a = $this->createKeyPair();
+        $action = new DLAction(Operation::GRANT_RIGHTS(), null, null, [], [], [$right], [$a]);
+        $attr = $this->createKeyPair();
+
+        return $this->api->distributionListAction($dl, $action, [$attr]);
+    }
+
+    public function RevokeSendAs(string $distListName, string $emailAccountName)
+    {
+        $grantee = new DistributionListGranteeSelector(GranteeType::USR(), DistributionListGranteeBy::NAME(), $emailAccountName);
+        $right = new DistributionListRightSpec("sendAsDistList", [$grantee]);
+        $dl = new DistributionListSelector(DistributionListBy::Name(), $distListName);
+        $a = $this->createKeyPair();
+        $action = new DLAction(Operation::GRANT_RIGHTS(), null, null, [], [], [$right], [$a]);
+        $attr = $this->createKeyPair();
+
+        return $this->api->distributionListAction($dl, $action, [$attr]);
+    }
+
+    public function GrantSendOnBehalfTo(string $distListName, string $emailAccountName)
+    {
+        $grantee = new DistributionListGranteeSelector(GranteeType::USR(), DistributionListGranteeBy::NAME(), $emailAccountName);
+        $right = new DistributionListRightSpec("sendOnBehalfOfDistList", [$grantee]);
+        $dl = new DistributionListSelector(DistributionListBy::Name(), $distListName);
+        $a = $this->createKeyPair();
+        $action = new DLAction(Operation::GRANT_RIGHTS(), null, null, [], [], [$right], [$a]);
+        $attr = $this->createKeyPair();
+
+        return $this->api->distributionListAction($dl, $action, [$attr]);
+    }
+
+    public function RevokeSendOnBehalfTo(string $distListName, string $emailAccountName)
+    {
+        $grantee = new DistributionListGranteeSelector(GranteeType::USR(), DistributionListGranteeBy::NAME(), $emailAccountName);
+        $right = new DistributionListRightSpec("sendOnBehalfOfDistList", [$grantee]);
+        $dl = new DistributionListSelector(DistributionListBy::Name(), $distListName);
+        $a = $this->createKeyPair();
+        $action = new DLAction(Operation::REVOKE_RIGHTS(), null, null, [], [], [$right], [$a]);
+        $attr = $this->createKeyPair();
+
+        return $this->api->distributionListAction($dl, $action, [$attr]);
+    }
+
+    public function getDistributionListMembership(string $distListName)
+    {
+        $limit = 0;
+        $offset = 0;
+
+        $dl = new DistributionListSelector(DistributionListBy::Name(), $distListName);
+
+        return $this->api->getDistributionListMembership($dl, $limit, $offset);
     }
 
     public function getAllAccounts() {
         $serverSelector = null;
-        $domainSelector = new \Zimbra\Admin\Struct\DomainSelector(\Zimbra\Enum\DomainBy::NAME(), $this->domain);
+        $domainSelector = new DomainSelector(DomainBy::NAME(), $this->domain);
+
         return $this->api->getAllAccounts($serverSelector, $domainSelector);
     }
 
-    public function modifyCoS($accountId, $cosId)
+    public function modifyCoS(string $emailAccountId, string $cosId)
     {
-        $attr = new \Zimbra\Struct\KeyValuePair('zimbraCOSId', $cosId);
-        return $this->api->modifyAccount($accountId, [$attr]);
+        $attr = $this->createKeyPair('zimbraCOSId', $cosId);
+        return $this->api->modifyAccount($emailAccountId, [$attr]);
     }
 
-    public function removeDistributionListMember($listId, $member)
+    public function removeDistributionListMember(string $distListId, string $emailAccountName)
     {
-        $dlms = [$member];
+        $dlms = [$emailAccountName];
         $accounts = [];
-        return $this->api->removeDistributionListMember($listId, $dlms, $accounts);
+
+        return $this->api->removeDistributionListMember($distListId, $dlms, $accounts);
     }
 
-    public function renameDistributionList($listId, $newName)
+    public function renameDistributionList(string $distListId, string $newName)
     {
-        return $this->api->renameDistributionList($listId, $newname);
+        return $this->api->renameDistributionList($distListId, $newName);
     }
 
-    public function getAccountById($id) 
-    {
-        $apllyCos = null;
-        $attr = ['uid','givenName','sn','mail','zimbraMailQuota','zimbraAccountStatus'];
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::ID(), $id);
-        return $this->api->getAccount($account, apllyCos, $attr)->account;
-    }
-
-    public function getAccountByName($name) 
+    public function getAccountById(string $emailAccountId)
     {
         $apllyCos = null;
-        $attr = ['uid','givenName','sn','mail','zimbraMailQuota','zimbraAccountStatus'];
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::NAME(), $name);
+
+        $attr = [
+            'sn',
+            'uid',
+            'mail',
+            'givenName',
+            'zimbraMailQuota',
+            'zimbraAccountStatus',
+        ];
+
+        $account = new AccountSelector(AccountBy::ID(), $emailAccountId);
+
         return $this->api->getAccount($account, $apllyCos, $attr)->account;
     }
 
-    public function getAccountMembershipbyName($name)
+    public function getAccountByName(string $emailAccountName)
     {
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::NAME(), $name);
-        return $this->api->getAccountMembership($account);
+        $apllyCos = null;
+
+        $attr = [
+            'sn',
+            'uid',
+            'mail',
+            'givenName',
+            'zimbraMailQuota',
+            'zimbraAccountStatus',
+        ];
+
+        $account = new AccountSelector(AccountBy::NAME(), $emailAccountName);
+
+        return $this->api->getAccount($account, $apllyCos, $attr)->account;
     }
-    
-    public function getAccountMembershipbyId($id)
+
+    public function getAccountMembershipByName(string $emailAccountName)
     {
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::ID(), $id);
+        $account = new AccountSelector(AccountBy::NAME(), $emailAccountName);
         return $this->api->getAccountMembership($account);
     }
 
-    public function getAccountInfoById($id) 
+    public function getAccountMembershipbyId(string $emailAccountId)
     {
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::ID(), $id);
+        $account = new AccountSelector(AccountBy::ID(), $emailAccountId);
+        return $this->api->getAccountMembership($account);
+    }
+
+    public function getAccountInfoById(string $emailAccountId)
+    {
+        $account = new AccountSelector(AccountBy::ID(), $emailAccountId);
         return $this->api->getAccountInfo($account);
     }
 
-    public function getAccountInfoByName($name) 
+    public function getAccountInfoByName($emailAccountName)
     {
-        $account = new \Zimbra\Struct\AccountSelector(\Zimbra\Enum\AccountBy::NAME(), $name);
+        $account = new AccountSelector(AccountBy::NAME(), $emailAccountName);
         return $this->api->getAccountInfo($account);
     }
 
@@ -147,8 +247,18 @@ class ZimbraApiClient
         return $this->api->getAllDomains()->domain;
     }
 
+    public function api()
+    {
+        return $this->api;
+    }
+
     public function __call($method, $parameters)
     {
         return $this->forwardCallTo($this->api, $method, $parameters);
+    }
+
+    private function createKeyPair($key = '', $value = '')
+    {
+        return new KeyValuePair($key, $value);
     }
 }
